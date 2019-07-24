bfcd4685ec1008dcab7fe847be1219f0f7b05d21
Bianca Gotaski <biancagotaski@outlook.com.br>
2019-07-24 18:53:41 -0300

    fixed the landing page css and html file

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,28 +6,29 @@ class Landing extends Component{
         return (
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="landing-grid">
-                    <Cell col={12}></Cell>
-                    <img src="https://pbs.twimg.com/profile_images/1128504038142689280/m0Bi_WOb_400x400.jpg"
-                    alt="avatar"
-                    className="avatar-img"></img>
+                    <Cell col={12}>
+                        <img src="https://pbs.twimg.com/profile_images/1128504038142689280/m0Bi_WOb_400x400.jpg"
+                        alt="avatar"
+                        className="avatar-img"></img>
 
-                    <div className="banner-text">
-                        <h1>Full Stack Web Developer</h1>
-                        <hr/>
-                        <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+                            <hr/>
+                            <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
 
-                        <div className="social-links">
-                            {/** GitHub */}
-                            <a href="https://github.com/biancagotaski" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-github-square" aria-hidden="true"></i>
-                            </a>
+                            <div className="social-links">
+                                {/** GitHub */}
+                                <a href="https://github.com/biancagotaski" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true"></i>
+                                </a>
 
-                            {/** LinkedIn */}
-                            <a href="https://br.linkedin.com/in/bianca-gotaski-b9bba284" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-                            </a>
+                                {/** LinkedIn */}
+                                <a href="https://br.linkedin.com/in/bianca-gotaski-b9bba284" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                                </a>
+                            </div>
                         </div>
-                    </div>
+                    </Cell>
                 </Grid>    
             </div>
         )
